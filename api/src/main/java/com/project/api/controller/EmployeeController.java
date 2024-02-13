d08c2a46d7f51d88ed474db1c01b154485a1a50c
LoicMadern <loicmaderns1@gmail.com>
2024-02-13 21:03:55 +0100

    allow cross domaine for API and clear unused script

--- api/src/main/java/com/project/api/controller/EmployeeController.java
+++ api/src/main/java/com/project/api/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin(origins = { "http://localhost:4200"})
 @RequestMapping("api/employees")
 public class EmployeeController {
 
